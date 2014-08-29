763d3a2edcfcbe426a74192135474efd0e869ca0
da99 <i-hate-spam-1234567@mailinator.com>
2014-08-29 14:57:07 -0500

    Removed: obsolete code from okdoki repo.

--- Okdoki/app.rb
+++ Okdoki/app.rb
@@ -1,68 +1,52 @@
+require 'multi_json'
 
-require 'content-security-policy'
-require 'sinatra'
-require 'rack/contrib'
-
-ContentSecurityPolicy.configure do |csp|
-  csp['default-src'] =  "'self' *.okdoki.com localhost";
-  csp['img-src']     =  "*"
+# start_server
+class C
+  include Server::Test
 end
 
-# -- Middleware --------------------------------------
-use Rack::Protection::SessionHijacking # 0
-use ContentSecurityPolicy              # 1
-use Rack::Protection::RemoteReferrer   # 2
-use Rack::PostBodyContentTypeParser    # 3
-use Rack::Protection::EscapedParams    # 4
-use Rack::Protection::RemoteReferrer   # 5
+client = CLIENT = C.new
 
-# --- Order matters because they set up middelware ---
-require './Server/Session/middleware'        # 0
-require './Server/Okdoki/middleware/Deserialize_User' # 0.1
-require './Server/Okdoki/middleware/As_This_Life'   # 0.2
-require './Server/Okdoki/Escape_All'     # 1
-require './Server/Okdoki/helpers/CSRF'           # 2
-require './Server/Okdoki/helpers/json'   # 3
-require './Server/Okdoki/helpers/html'    # 4
-require './Server/Okdoki/middleware/Bool_Params'    # 5
-require './Server/Okdoki/middleware/No_Cache'       # 6
-# ----------------------------------------------------
-
-# --- Siantra Helpers --------------------------------
-require './Server/Okdoki/helpers/logic_for'
-require './Server/Session/helpers'
-# ----------------------------------------------------
-
-# --- Init the DB Connection -------------------------
-require './Server/Okdoki/model'
-
-# --- The Models -------------------------------------
-%w{ Customer Screen_Name }.each { |m|
-  require "./Server/#{m}/model"
-}
-
-# --- The Routes -------------------------------------
-require "./Server/Okdoki/top_slash/routes"
-require "./Server/Session/routes"
-require "./Server/Customer/routes"
-require "./Server/Screen_Name/routes"
-
-
-# --- Error Handling ---------------------------------
-NOT_FOUND = File.read("./Public/errors/404.html")
-ERROR_50x = File.read("./Public/errors/50x.html")
-
-not_found do
-  NOT_FOUND.sub('{{path}}', request.path_info)
+def GET *args
+  CLIENT.GET *args
 end
 
-error do
-  ERROR_50x.sub('{{path}}', request.path_info) # env['sinatra.error'].name
+def POST *args
+  CLIENT.POST *args
 end
-# ----------------------------------------------------
+
+def _csrf
+  CLIENT._csrf
+end
+
+describe "/" do
+
+  it "returns a 200" do
+    GET('/').code.to_i.should == 200
+  end
+
+  it "includes a _csrf value" do
+    (GET('/')/ "#CSRF").size.should == 1
+  end
+
+end # === describe it runs ===
 
 
+describe "POST /user" do
 
+  it "raises an error if no _csrf token passed" do
+    lambda {
+      r = POST( "/user", {'screen_name'=>'ted1', 'pass_word'=>'this is mey  passw'}, 'X-CSRF-Token' => nil)
+    }.should.raise(Mechanize::ResponseCodeError).
+      response_code.to_i.should == 500
+  end
 
+  it "tells user confirm_pass_phrase is required." do
+    r = POST( "/user", 'screen_name'=>'ted1', 'pass_word'=>'this is mey  passw')
+    reply = MultiJson.load(r.body)
+    reply['success'].should == false
+    reply['msg'].should == "Pass phrase confirmation is required."
+  end
 
+end # === describe
 
