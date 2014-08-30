5fafcb057b6df4258d678571ee3f939d60738fe2
da99 <i-hate-spam-1234567@mailinator.com>
2014-08-29 19:02:10 -0500

    Imported: old code from okdoki repo.

--- lib/datoki.rb
+++ lib/datoki.rb
@@ -1,7 +1,14 @@
 
 module Datoki
 
+  UTC_NOW_RAW  = "timezone('UTC'::text, now())"
+  UTC_NOW      = ::Sequel.lit("timezone('UTC'::text, now())")
+  UTC_NOW_DATE = ::Sequel.lit("CURRENT_DATE")
+
   Invalid = Class.new RuntimeError
+
+  Actions = [:create, :read, :update, :update_or_create, :trash, :delete]
+
   class << self
 
     def included klass
@@ -16,6 +23,7 @@ module Datoki
     def initialize_def_field
       @fields = {}
       @current_field = nil
+      @current_on = nil
     end
 
     def fields
@@ -24,8 +32,18 @@ module Datoki
 
     def field name
       @current_field = name
-      @fields[@current_field] = {}
+
+      @fields[@current_field] ||= {
+        :on           => {:all=>[]},
+        :english_name => name.to_s.freeze
+      }
+
+      @current_on = current_field[:on][:all]
       yield
+    end
+
+    def current_on
+      @current_on
     end
 
     def current_field
@@ -52,7 +70,64 @@ module Datoki
       self
     end
 
+    def on *actions
+      actions.each { |name|
+
+        fail "Invalid action: #{name.inspect}" unless Actions.include? name
+        current_field[:on][name] ||= []
+
+        orig = current_on
+        @current_on = current_field[:on][name]
+        yield
+        @current_on = orig
+      }
+
+      self
+    end
+
+    %{
+      required
+      be
+      strip
+      upcase
+      to_i
+      set_to
+      equal_to
+      nil_if_empty
+      match
+      not_match
+      one_of_these
+    }.each { |name|
+      eval <<-EOF
+        def #{name} *args
+          common_on << [:#{name}, *args]
+        end
+      EOF
+    }
+
+    def min i
+      current_on << [:min, Integer(i)]
+    end
+
+    def max i
+      current_on << [:max, Integer(i)]
+    end
+
+    def within min, max
+      current_on << [:range, Integer(min) - 1, Integer(max) - 1]
+    end
+
+    def min_max min, max
+      current_on << [:range, Integer(min), Integer(max)]
+    end
+
   end # === Def_Field
+
+  def create h
+    r = new
+    r.create h
+    r
+  end
 
   # ================= Instance Methods ===============
 
